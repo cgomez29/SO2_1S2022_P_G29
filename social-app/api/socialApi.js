58d7f0bc126d9bf906c59d5fb7bf9687de8d6af2
201801480-Cristian <2912196670108@ingenieria.usac.edu.gt>
2022-04-29 16:21:10 -0600

    [FIX] socialAPI

--- social-app/api/socialApi.js
+++ social-app/api/socialApi.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 let socialApi = axios.create({
-  baseURL: process.env.API_URL,
-  //baseURL: 'http://localhost:4000/api',
+  //baseURL: process.env.API_URL,
+  baseURL: 'http://localhost:4000/api',
 });
 
 export default socialApi;
