a78b9efd0414c0d858fe4fdca857bd0ad96e4d31
Sevenpok <david20924col@gmail.com>
2022-04-29 15:59:15 -0600

    Se arreglo variable de entorno para el entorno de produccion

--- social-app/api/socialApi.js
+++ social-app/api/socialApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let socialApi = axios.create({
-  baseURL: process.env.API_URL,
+  baseURL: process.env.NEXT_PUBLIC_URL,
   //baseURL: 'http://localhost:4000/api',
 });
 
